4f2690f6c304639c2eb0c8a5dea4e06b59653fac
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-02-09 19:20:15 +0530

    added auto login

---
+++ cookies.py
@@ -0,0 +1,32 @@
+cookies = [
+    {
+        'domain': '.instagram.com',
+        'httpOnly': True,
+        'name': 'rur',
+        'path': '/',
+        'sameSite': 'Lax',
+        'secure': True,
+        'value': '"LDC\\0549122788414\\0541707480072:01f7bad173c1e66cff2be163c0689d4fdd8d9e47dc6e34472e9692297690af7daf4ed5dc"'
+    },
+
+    {'domain': '.instagram.com', 'expiry': 1676548751, 'httpOnly': True, 'name': 'shbid', 'path': '/', 'sameSite': 'Lax', 'secure': True,
+               'value': '"17205\\0549122788414\\0541707479951:01f7bf8ea003434dc20b4534e09be7245185dbf0d100c28dffd9c8156235ab05fd22becb"'},
+    {'domain': '.instagram.com', 'expiry': 1676548751, 'httpOnly': True, 'name': 'shbts', 'path': '/', 'sameSite': 'Lax', 'secure': True,
+               'value': '"1675943951\\0549122788414\\0541707479951:01f7ed1b23e5c1b64654ac9c194a3e0e91a00206061023218aa727d507c0a46c48beb244"'},
+    {'domain': '.instagram.com', 'expiry': 1707479950, 'httpOnly': True, 'name': 'sessionid', 'path': '/', 'sameSite': 'Lax',
+     'secure': True, 'value': '9122788414%3ASttCfzFKVSBf9t%3A0%3AAYc4mfVveBoo__xy6RC3Veu7CoEAnQ4l8Vc5ecd-dg'},
+    {'domain': '.instagram.com', 'expiry': 1707393672, 'httpOnly': False, 'name': 'csrftoken',
+     'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': 'RNupBwx2uclFNSOBSCvZVnAgZaOuqbKQ'},
+    {'domain': '.instagram.com', 'expiry': 1710503934, 'httpOnly': True, 'name': 'datr',
+     'path': '/', 'sameSite': 'None', 'secure': True, 'value': '_N_kY5ULxBR05GHwaIFuHa3B'},
+    {'domain': '.instagram.com', 'expiry': 1683720072, 'httpOnly': False, 'name': 'ds_user_id',
+     'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '9122788414'},
+    {'domain': '.instagram.com', 'expiry': 1707479934, 'httpOnly': True, 'name': 'ig_did', 'path': '/',
+     'sameSite': 'None', 'secure': True, 'value': 'C1AE0AB9-2EB8-4CB3-8940-005A232885D4'},
+    {'domain': '.instagram.com', 'expiry': 1707479934, 'httpOnly': False,
+     'name': 'ig_nrcb', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '1'},
+    {'domain': '.instagram.com', 'expiry': 1710503934, 'httpOnly': False, 'name': 'mid',
+     'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': 'Y-Tf_QALAAEnvUG8DXKzWJpOX_2O'},
+    {'domain': '.instagram.com', 'expiry': 1676548861, 'httpOnly': False,
+        'name': 'dpr', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '1.25'}
+]
--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from dotenv import load_dotenv
 from os import getenv
+from cookies import cookies
 
 
 load_dotenv(dotenv_path="config.env")
+
+
 down_time = 4
 loading_rounds = 0
 limit = getenv("LIMIT")
@@ -25,34 +28,54 @@ class InstaBot:
 
 
     def __init__(self):
+
         self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
         self.target = getenv("TARGET_USERNAME")
+
+        self.cookies_exists = False
+
+
+    def set_cookies_in_browser(self):
+
+        if cookies != []:
+
+            for cookie in cookies:
+                self.browser.add_cookie(cookie)
+                
+            self.cookies_exists = True
 
 
     def login(self):
 
         self.browser.get("https://www.instagram.com")
+
+        self.set_cookies_in_browser()
+
         sleep(down_time)
 
-        username = self.browser.find_element(By.NAME, "username")
-        password = self.browser.find_element(By.NAME, "password")
-        submitbtn = self.browser.find_element(By.TAG_NAME, "button")
+        if self.cookies_exists == False:
 
-        try:
-            username.send_keys(getenv("INSTAGRAM_USERNAME"))
+            username = self.browser.find_element(By.NAME, "username")
+            password = self.browser.find_element(By.NAME, "password")
+            submitbtn = self.browser.find_element(By.TAG_NAME, "button")
+
+            try:
+                username.send_keys(getenv("INSTAGRAM_USERNAME"))
+                sleep(down_time)
+
+                password.send_keys(getenv("INSTAGRAM_PASSWORD"))
+                sleep(down_time)
+            
+            except:
+                print("Enter username, password in config file first and then retry")
+                print("Exiting")
+                exit()
+
+            submitbtn.click()
+            
             sleep(down_time)
-
-            password.send_keys(getenv("INSTAGRAM_PASSWORD"))
-            sleep(down_time)
-        
-        except:
-            print("Enter username, password in config file first and then retry")
-            print("Exiting")
-            exit()
-
-        submitbtn.click()
-        sleep(down_time)
-        sleep(3)
+            sleep(3)
 
 
     def get_followers(self): 
@@ -122,8 +145,12 @@ class InstaBot:
 
 
     def main(self):
+
         self.login()
         self.like_story()
+
+        if self.cookies_exists == False:
+            print(self.browser.get_cookies())
 
 
 bot = InstaBot()
